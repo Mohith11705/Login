7e2b119e68fbcee1714e9e651033b3f64d9d3fe6
Mohith11705 <mohithit2023@ksrce.ac.in>
2025-07-03 15:39:09 +0530

    initial

--- tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-    const token=jwt.sign({email:user.email,password:user.password},key,{expiresIn:'1h'});
-//     res.status(200).send({token});
-// })
-// app.get('/home',authtoken,(req,res)=>{
-//     res.sendFile(path.join(__dirname,'views','home.html'))
-// })
